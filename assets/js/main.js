45d2870690d2e7149cd295a8966ae81af9dca085
takumi1407 <89883845+takumi1407@users.noreply.github.com>
2021-09-15 09:12:28 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,7 +26,7 @@ const app = Vue.createApp({
           // q3: 'おおお'
         },
         stage3: {
-          q1: 'ONE',
+          q1: 'EIGHT',
           // q2: 'かかか',
           // q3: 'ききき',
         }
